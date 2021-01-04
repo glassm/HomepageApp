d519c85b1bf9cd6c1e15a79d95fa5b70475f95a0
mark <mark.glass@gmail.com>
2021-01-03 22:58:53 -0500

    database, groups and links. Need sensor data

--- SQL/4_insert_data_to_link_group.sql
+++ SQL/4_insert_data_to_link_group.sql
@@ -1,13 +1,13 @@
-INSERT INTO link_group(title) values ('news');
-INSERT INTO link_group(title) values ('angular');
-INSERT INTO link_group(title) values ('javascript');
-INSERT INTO link_group(title) values ('pharmacy');
-INSERT INTO link_group(title) values ('doctors');
-INSERT INTO link_group(title) values ('spiritual');
+INSERT INTO link_group(group_title) values ('news');
+INSERT INTO link_group(group_title) values ('angular');
+INSERT INTO link_group(group_title) values ('javascript');
+INSERT INTO link_group(group_title) values ('pharmacy');
+INSERT INTO link_group(group_title) values ('doctors');
+INSERT INTO link_group(group_title) values ('spiritual');
 
-INSERT INTO links(fk_group_nr, title, url) values (1, 'CNN', 'http://cnn.com');
-INSERT INTO links(fk_group_nr, title, url) values (1, 'MSNBC', 'http://msnbc.com');
-INSERT INTO links(fk_group_nr, title, url) values (2, 'Medium.com', 'http://medium.com');
-INSERT INTO links(fk_group_nr, title, url) values (2, 'Angular IO', 'http://angular.io');
+INSERT INTO links(group_nr, description, url) values (1, 'CNN', 'http://cnn.com');
+INSERT INTO links(group_nr, description, url) values (1, 'MSNBC', 'http://msnbc.com');
+INSERT INTO links(group_nr, description, url) values (2, 'Medium.com', 'http://medium.com');
+INSERT INTO links(group_nr, description, url) values (2, 'Angular IO', 'http://angular.io');
 
 
